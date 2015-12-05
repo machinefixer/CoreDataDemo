207c8c27e1ae8720654691b06a91e4c1d4acf35f
machinefixer <waywardson.wayne@gmail.com>
2015-12-05 19:31:00 +0800

    complete demo

--- CoreDataDemo/ViewController.m
+++ CoreDataDemo/ViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ViewController.h"
+#import "AppDelegate.h"
+
+static NSString * const kLineEntityName = @"Line";
+static NSString * const kLineNumberKey = @"lineNumber";
+static NSString * const kLineTextKey = @"lineText";
 
 @interface ViewController ()
+
+@property (nonatomic, strong) IBOutletCollection(UITextField) NSArray *lineFields;
 
 @end
 
@@ -16,12 +23,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    
+    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:kLineEntityName];
+    
+    NSError *error;
+    NSArray *objects = [context executeFetchRequest:request error:&error];
+    if (objects == nil) {
+        NSLog(@"查询失败");
+    }
+    
+    for (NSManagedObject *oneObject in objects) {
+        int lineNum = [[oneObject valueForKey:kLineNumberKey] intValue];
+        NSString *lineText = [oneObject valueForKey:kLineTextKey];
+        
+        UITextField *theField = self.lineFields[lineNum];
+        theField.text = lineText;
+    }
+    
+    UIApplication *app = [UIApplication sharedApplication];
+    [[NSNotificationCenter defaultCenter]
+     addObserver:self
+     selector:@selector(applicationWillResignActive:)
+     name:UIApplicationWillResignActiveNotification
+     object:app];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)applicationWillResignActive:(NSNotification *)notification
+{
+    AppDelegate *appdelegate = [UIApplication sharedApplication].delegate;
+    NSManagedObjectContext *context = [appdelegate managedObjectContext];
+    NSError *error;
+    for (int i = 0; i < 4; i++) {
+        UITextField *theField = self.lineFields[i];
+        NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:kLineEntityName];
+        NSPredicate *pred = [NSPredicate predicateWithFormat:@"(%K = %d)", kLineNumberKey, i];
+        [request setPredicate:pred];
+        
+        NSArray *objects = [context executeFetchRequest:request error:&error];
+        if (objects == nil) {
+            NSLog(@"出错了");
+        }
+        
+        NSManagedObject *theLine = nil;
+        if ([objects count] > 0) {
+            theLine = [objects objectAtIndex:0];
+        }
+        else {
+            theLine = [NSEntityDescription
+                       insertNewObjectForEntityForName:kLineEntityName
+                       inManagedObjectContext:context];
+        }
+        
+        [theLine setValue:[NSNumber numberWithInt:i] forKey:kLineNumberKey];
+        [theLine setValue:theField.text forKey:kLineTextKey];
+    }
 }
 
 @end
